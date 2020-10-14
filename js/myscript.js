e9edd570b5cc753a74ecbe3a80c322676f93d0a7
gptshubham595 <gptshubham595@gmail.com>
2020-10-15 04:00:17 +0530

    Task 1 Done

---
+++ js/myscript.js
@@ -0,0 +1,14 @@
+var clicks = 0;
+var a = document.getElementById("bookmark");
+var rC = document.getElementById("relevanceColor");
+rC.style.color = "lightgreen";
+function go() {
+    clicks++;
+    if (clicks % 2 === 1) {
+        a.style.color = "lightgreen";
+    }
+    else {
+        a.style.color = "gray";
+    }
+};
+
---
+++ js/titleid.js
@@ -0,0 +1,23 @@
+var news = [
+            {
+                "id":"images/foxA.jpg",
+                 "title":"Greenland's Viral image",
+                 "desc":"AAAA:  A photo showing sled dogs wading through water ankle-deep on top of melting ice sheet in Greenland has gone viral. The picture has taken by Steffen Oslen from the Centre for Ocean and Ice at the Danish Meterological Institute shows the scale of melting ice in Greenland. Oslen said the ice sheet beneath the water was 1.2 metres thick.",
+                 "shortdesc":"Viral image of greenland",
+                 "shot":"Shot by Ankush Verma."
+            },
+            {
+                "id":"images/foxB.jpg",
+                 "title":"Lorem Ipsum",
+                 "desc":" BBBBB: A photo showing sled dogs wading through water ankle-deep on top of melting ice sheet in Greenland has gone viral. The picture has taken by Steffen Oslen from the Centre for Ocean and Ice at the Danish Meterological Institute shows the scale of melting ice in Greenland. Oslen said the ice sheet beneath the water was 1.2 metres thick.",
+                "shortdesc":"Lorem Ipsum dolor sit amet, consetcur",
+                 "shot":"Shot by Ankush Verma."
+            },
+            {
+                "id":"images/foxC.jpg",
+                 "title":"Dolor sit amet,consectetur",
+                 "desc":" CCCC: A photo showing sled dogs wading through water ankle-deep on top of melting ice sheet in Greenland has gone viral. The picture has taken by Steffen Oslen from the Centre for Ocean and Ice at the Danish Meterological Institute shows the scale of melting ice in Greenland. Oslen said the ice sheet beneath the water was 1.2 metres thick.",
+                "shortdesc":"Lorem Ipsum dolor sit amet, consetcur",
+                 "shot":"Shot by Ankush Verma."
+            }
+           ];
