0380969bbe476345c41d70efaf983db100afccd5
gptshubham595 <gptshubham595@gmail.com>
2020-10-15 16:55:30 +0530

    Task2 search

--- Task2/go-rest-api/main.go
+++ Task2/go-rest-api/main.go
@@ -6,8 +6,11 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strings"
+	"unicode"
 
 	"github.com/gorilla/mux"
+	"github.com/gorilla/schema"
 )
 
 type article struct {
@@ -27,7 +30,7 @@ var articles = allArticles{
 }
 
 func homeLink(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome home! \n \n GET: /articles gives all articles \n\n POST: /articles with Body>Raw as {\"id\":\"NUMBER\",\"title\":\"Give your title\",\"description\":\"GIve your Description\",} \n\n DELETE:/articles/2 to delete article number 2 \n\n PATCH: /articles/1 in BODY>raw {\"title\":\"Write new title for the article\",\"description\":\"Write here updated description for the article\"}")
+	fmt.Fprintf(w, "Welcome home! \n \n GET: /articles gives all articles \n\n POST: /articles with Body>Raw as {\"id\":\"NUMBER\",\"title\":\"Give your title\",\"description\":\"GIve your Description\",} \n\n DELETE:/articles/2 to delete article number 2 \n\n PATCH: /articles/1 in BODY>raw {\"title\":\"Write new title for the article\",\"description\":\"Write here updated description for the article\"} \n SEARCH: /articles/search/<query> It will List every where found")
 }
 
 func createArticle(w http.ResponseWriter, r *http.Request) {
@@ -48,31 +51,46 @@ func createArticle(w http.ResponseWriter, r *http.Request) {
 	// Return the newly created event
 	json.NewEncoder(w).Encode(newArticle)
 }
+
+var decoder = schema.NewDecoder()
+
 func searchArticle(w http.ResponseWriter, r *http.Request) {
-	vals := r.URL.Query()
-	log.Println(vals)
-	log.Println(vals["q"])
-	// Get the ID from the url
 	articleID := mux.Vars(r)["id"]
-	var newArticle article
-	articles = append(articles, newArticle)
-	// Get the details from an existing event
-	// Use the blank identifier to avoid creating a value that will not be used
+	words := []string{articleID}
 	for _, singleArticle := range articles {
 		if singleArticle.ID == articleID {
-			// json.NewEncoder(w).Encode(singleArticle)
-			articles = append(articles, newArticle)
+			json.NewEncoder(w).Encode("FOUND AT ID:")
+			json.NewEncoder(w).Encode(singleArticle)
+
 		}
-		if singleArticle.Title == articleID {
-			// json.NewEncoder(w).Encode(singleArticle)
-			articles = append(articles, newArticle)
+		if len(WordsInSentence(words, singleArticle.Title)) > 0 {
+			json.NewEncoder(w).Encode("FOUND AT TITLE")
+			json.NewEncoder(w).Encode(singleArticle)
 		}
-		if singleArticle.Description == articleID {
-			// json.NewEncoder(w).Encode(singleArticle)
-			articles = append(articles, newArticle)
+		if len(WordsInSentence(words, singleArticle.Description)) > 0 {
+			json.NewEncoder(w).Encode("FOUND AT Description:")
+			json.NewEncoder(w).Encode(singleArticle)
 		}
 	}
-	json.NewEncoder(w).Encode(newArticle)
+
+}
+func WordsInSentence(words []string, sentence string) []string {
+	var in []string
+
+	dict := make(map[string]string, len(words))
+	for _, word := range words {
+		dict[strings.ToLower(word)] = word
+	}
+
+	f := func(r rune) bool { return !unicode.IsLetter(r) }
+	for _, word := range strings.FieldsFunc(sentence, f) {
+		if word, ok := dict[strings.ToLower(word)]; ok {
+			in = append(in, word)
+			delete(dict, word)
+		}
+	}
+
+	return in
 }
 
 func getOneArticle(w http.ResponseWriter, r *http.Request) {
@@ -82,6 +100,7 @@ func getOneArticle(w http.ResponseWriter, r *http.Request) {
 	// Get the details from an existing event
 	// Use the blank identifier to avoid creating a value that will not be used
 	for _, singleArticle := range articles {
+
 		if singleArticle.ID == articleID {
 			json.NewEncoder(w).Encode(singleArticle)
 		}
@@ -134,7 +153,7 @@ func main() {
 	router.HandleFunc("/articles", createArticle).Methods("POST")
 	router.HandleFunc("/articles", getAllArticle).Methods("GET")
 	router.HandleFunc("/articles/{id}", getOneArticle).Methods("GET")
-	router.HandleFunc("/articles/search?q={id}", searchArticle).Methods("GET")
+	router.HandleFunc("/articles/search/{id}", searchArticle).Methods("GET")
 	router.HandleFunc("/articles/{id}", updateArticle).Methods("PATCH")
 	router.HandleFunc("/articles/{id}", deleteArticle).Methods("DELETE")
 	log.Fatal(http.ListenAndServe(":8080", router))
---
+++ Task2/go-rest-api/pagination.go
@@ -0,0 +1,15 @@
+package main
+
+type Pagination struct {
+	Limit     int         `json:"limit"`
+	Page      int         `json:"page"`
+	Sort      string      `json:"sort"`
+	TotalRows int         `json:"total_rows"`
+	FirstPage string      `json:"first_page"`
+	PrevPage  string      `json:"prev_page"`
+	NextPage  string      `json:"next_page"`
+	LastPage  string      `json:"last_page"`
+	FromRow   int         `json:"from_row"`
+	ToRow     int         `json:"to_row"`
+	Rows      interface{} `json:"rows"`
+}
