56b0b95c5add1c5109c3bf3a57ff80e943511e2b
Shubham gupta Ggps <24877361+gptshubham595@users.noreply.github.com>
2020-10-15 17:44:14 +0530

    Create main_test.go

---
+++ Task2/go-rest-api/main_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestCalculate(t *testing.T) {
+	req, err := http.NewRequest("GET", "/articles/1", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	q := req.URL.Query()
+	q.Add("id", "1")
+	req.URL.RawQuery = q.Encode()
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(getOneArticle)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	// Check the response body is what we expect.
+	expected := `{"ID":"1","Title":"First Article","Description":"Come First join us for a chance to learn how golang works and get to eventually try it out"}`
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
+}
